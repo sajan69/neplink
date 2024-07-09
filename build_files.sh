9de165b7adb8316186ad276723d18c5c23bacbcd
Sajan Adhikari <120996556+sajan69@users.noreply.github.com>
2024-07-09 12:23:24 +0545

    live sql server

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
+echo " BUILD START"
+
+python3.9 --version
+python3.9 -m ensurepip # não aparece nos tutoriais internacionais
+python3.9 -m pip install -r requirements.txt
+
+echo " MAKE MIGRATIONS..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+
+python3.9 manage.py collectstatic --noinput --clear
+echo " BUILD END"
