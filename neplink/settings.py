80e00a9377d7bb5d0de947cf3f24cadd4e4145ac
Sajan Adhikari <120996556+sajan69@users.noreply.github.com>
2024-07-09 12:38:52 +0545

    live sql server

--- neplink/settings.py
+++ neplink/settings.py
@@ -148,13 +148,13 @@ LOGOUT_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
