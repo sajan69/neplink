c5b1f8dba28a566a57028eff927dcfceca3c87bd
Sajan Adhikari <120996556+sajan69@users.noreply.github.com>
2024-07-11 15:11:27 +0545

    api opening in url fixed by name

--- posts/urls.py
+++ posts/urls.py
@@ -19,12 +19,12 @@ urlpatterns = [
 
     #Api views
     path('api/posts/', api_views.PostListCreateAPIView.as_view(), name='post_list_create'),
-    path('api/posts/<int:pk>/', api_views.PostDetailAPIView.as_view(), name='post_detail'),
-    path('api/posts/<int:post_id>/like/', api_views.LikePostAPIView.as_view(), name='like_post'),
-    path('api/posts/<int:post_id>/comments/', api_views.CommentCreateAPIView.as_view(), name='comment_create'),
-    path('api/comments/<int:pk>/', api_views.CommentDetailAPIView.as_view(), name='comment_detail'),
-    path('api/comments/<int:comment_id>/like/', api_views.LikeCommentAPIView.as_view(), name='like_comment'),
-    path('api/comments/<int:comment_id>/replies/', api_views.CommentReplyCreateAPIView.as_view(), name='reply_create'),
-    path('api/replies/<int:pk>/', api_views.CommentReplyDetailAPIView.as_view(), name='reply_detail'),
-    path('api/replies/<int:reply_id>/like/', api_views.LikeReplyAPIView.as_view(), name='like_reply'),
+    path('api/posts/<int:pk>/', api_views.PostDetailAPIView.as_view(), name='post_detail_api'),
+    path('api/posts/<int:post_id>/like/', api_views.LikePostAPIView.as_view(), name='like_post_api'),
+    path('api/posts/<int:post_id>/comments/', api_views.CommentCreateAPIView.as_view(), name='comment_create_api'),
+    path('api/comments/<int:pk>/', api_views.CommentDetailAPIView.as_view(), name='comment_detail_api'),
+    path('api/comments/<int:comment_id>/like/', api_views.LikeCommentAPIView.as_view(), name='like_comment_api'),
+    path('api/comments/<int:comment_id>/replies/', api_views.CommentReplyCreateAPIView.as_view(), name='reply_create_api'),
+    path('api/replies/<int:pk>/', api_views.CommentReplyDetailAPIView.as_view(), name='reply_detail_api'),
+    path('api/replies/<int:reply_id>/like/', api_views.LikeReplyAPIView.as_view(), name='like_reply_api'),
 ]
