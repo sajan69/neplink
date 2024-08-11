96ec47655ceccc215c6327b60d0cbb7f09477b27
Sajan Adhikari <120996556+sajan69@users.noreply.github.com>
2024-08-11 14:11:05 +0545

    cloudinary url not working

--- neplink/settings.py
+++ neplink/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-    'cloudinary_storage.middleware.CloudinaryFileUploadMiddleware',
+  
 
 ]
 
